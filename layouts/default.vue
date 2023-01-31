b0d88b0e721c1ae1da4396bbf39248cf2032935d
emsbe <emma-sophie.betz@web.de>
2023-01-31 11:31:53 +0000

    added default layout for header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>  
-    <div>    
-        <!-- Some default layout shared across all pages -->
+    <div>
+        <header>
+            <NuxtLink to="/">
+                <img class="m-3" src="~/assets/img/rect24328.png" width="100" height="100" alt="POLLmelo logo" />
+            </NuxtLink>
+            <hr class="h-px my-8 bg-gray-500 border-0 dark:bg-gray-800">
+        </header>    
         <slot />  
     </div>
 </template>
