b3625a6fe7b24da2ad4b0f857f4b75f6a43c1b04
emsbe <emma-sophie.betz@web.de>
2023-01-31 15:02:01 +0000

    Add banner and banner title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-
+        <div class="relative">
+            <img class="object-cover h-60 w-full" src="~/assets/img/banner.png" alt="background-banner" />
+            <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                Jetzt abstimmen!</h1>
+        </div>
     </div>
 </template>
 
