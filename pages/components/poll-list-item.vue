dc6605497262a5746281a4eec967258dc51f1df8
martincodes-de <62392843+martincodes-de@users.noreply.github.com>
2023-02-03 00:04:44 +0100

    passed props for testing

--- pages/components/poll-list-item.vue
+++ pages/components/poll-list-item.vue
@@ -1,5 +1,10 @@
 <template>
-  <PollListItem></PollListItem>
+  <PollListItem
+      :title="'Neue Features hinzufügen'"
+      :description="'Lasst uns neue Features erstellen, kann ja nicht so schwierig sein. ' +
+       'Informatik ist doch nur wenn, dann xyz'"
+      :end-date="'2023-12-24'"
+  />
 </template>
 
 <script>
