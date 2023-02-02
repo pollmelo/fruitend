e8a9ac51c660fe4680581ef1a50895adf2539136
martincodes-de <62392843+martincodes-de@users.noreply.github.com>
2023-02-02 23:51:43 +0100

    add props

--- components/PollListItem.vue
+++ components/PollListItem.vue
@@ -7,20 +7,22 @@
             src="https://api.dicebear.com/5.x/micah/svg?seed=Peter&mouth=pucker,laughing,smile&size=64"
             alt="Avatar of poll creator"
         />
-        <h5 class="inline-block ml-2 text-lg font-semibold align-middle">Neues Feature</h5>
+        <h5 class="inline-block ml-2 text-lg font-semibold align-middle">{{ this.title }}</h5>
       </div>
 
       <Icon :class="['basis-10', 'h-10', 'text-green-400']" name="bi:circle-fill" :size="'5em'" />
     </div>
 
-    <p class="mb-3 truncate">Dies ist ein neues Feature, das wir brauchen. Bitte lasst es uns implementieren.</p>
+    <p class="mb-3 truncate">{{ this.description }}</p>
 
     <span>endet am 31.12.2023</span>
   </article>
 </template>
 
 <script>
-
+  export default {
+    props: ['title', 'description', 'endDate'],
+  }
 </script>
 
 <style scoped>
