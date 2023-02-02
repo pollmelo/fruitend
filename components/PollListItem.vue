8cbda73cf25375b397cb753755cba2b9370f2625
martincodes-de <62392843+martincodes-de@users.noreply.github.com>
2023-02-02 22:38:20 +0100

    updated alt attribute of user avater

--- components/PollListItem.vue
+++ components/PollListItem.vue
@@ -5,12 +5,14 @@
         <img
             class="inline-block rounded-full bg-slate-300 w-10 h-10"
             src="https://api.dicebear.com/5.x/micah/svg?seed=Peter&mouth=pucker,laughing,smile&size=64"
-            alt="Avatar image"
+            alt="Avatar of poll creator"
         />
         <h5 class="inline-block ml-2 text-lg font-semibold">Neues Feature</h5>
       </div>
     </div>
+
     <p class="mb-3">Dies ist ein neues Feature, das wir brauchen. Bitte lasst es uns implementieren.</p>
+
     <span>endet am 31.12.2023</span>
   </article>
 </template>
