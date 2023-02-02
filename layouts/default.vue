9761b6a20b148f2a3551ba5dab4830f08639b645
Malte Eisermann <online@eisermann.xyz>
2023-02-02 15:50:14 +0100

    added button with styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>  
     <div>
-        <header>
-            <NuxtLink to="/">
-                <img class="m-3" src="~/assets/img/rect24328.png" width="100" height="100" alt="POLLmelo logo" />
-            </NuxtLink>
-            <hr class="h-px my-8 bg-gray-500 border-0 dark:bg-gray-800">
-        </header>    
-        <slot />  
+      <header>
+        <NuxtLink to="/">
+          <img alt="POLLmelo logo" class="m-3" height="100" src="~/assets/img/rect24328.png" width="100"/>
+        </NuxtLink>
+        <hr class="h-px my-8 bg-gray-500 border-0 dark:bg-gray-800">
+      </header>
+      <div class="container mx-auto p-4">
+        <slot/>
+      </div>
     </div>
 </template>
