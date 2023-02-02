d8b45e9e5a949b351326882bc4f759ecbfd0d349
Malte Eisermann <online@eisermann.xyz>
2023-02-02 23:02:27 +0100

    started on the form

--- pages/polls/create.vue
+++ pages/polls/create.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
+      <form class="w-full max-w-sm mx-auto">
 
+        <input
+            id="name"
+            class="bg-gray-200 border-2 border-[#E95829] w-full py-2 px-4"
+            type="text" value="*Titel eingeben" required>
+
+        <input
+            id="description"
+            class="bg-gray-200 border-2 border-[#E95829] w-full py-2 px-4"
+            type="text" value="*Beschreibung" required>
+        <button
+            class="submit"
+            type="button">
+          Veröffentlichen
+        </button>
+      </form>
     </div>
 </template>
 
