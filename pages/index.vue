1f9d40a9f3b05313277314b11aad2023174fc6ae
martincodes <martin.schneider@techniknews.net>
2023-02-07 15:59:51 +0100

    remove useless comment

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
     <div>
       <div class="relative">
         <img alt="background-banner" class="object-cover h-60 w-full" src="~/assets/img/banner.png"/>
-        <!-- webp anstatt png -->
         <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           Jetzt abstimmen!</h1>
       </div>
