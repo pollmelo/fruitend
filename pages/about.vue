19a3e5fa42b49e125ecf1eb31600e5f8e8d9f3b1
Malte Eisermann <online@eisermann.xyz>
2023-02-13 09:42:02 +0100

    added team section

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-
+    <h1 class="majorTxt m-4">Team</h1>
+    <div class="flex gap-10 justify-center mx-auto">
+      <div>
+        <img src="~/assets/img/teamBlank.png" alt="icon of a person">
+        <p class="minorTxt">Martin</p>
+        <p class="text-gray-400">Technical Team Lead</p>
+        <NuxtLink to="https://github.com/martincodes-de"><img src="~/assets/img/github-mark.png" alt="GitHub"></NuxtLink>
+      </div>
+      <div>
+        <img src="~/assets/img/teamBlank.png" alt="icon of a person">
+        <p class="minorTxt">Emma</p>
+        <p class="text-gray-400">Design / FullStack Developerin</p>
+        <NuxtLink to="https://github.com/emsbe"><img src="~/assets/img/github-mark.png" alt="GitHub"></NuxtLink>
+      </div>
+      <div>
+        <img src="~/assets/img/teamBlank.png" alt="icon of a person">
+        <p class="minorTxt">Nik</p>
+        <p class="text-gray-400">FullStack Developer</p>
+        <NuxtLink to="https://github.com/NiklasBGit"><img src="~/assets/img/github-mark.png" alt="GitHub"></NuxtLink>
+      </div>
+      <div>
+        <img src="~/assets/img/teamBlank.png" alt="icon of a person">
+        <p class="minorTxt">Malte</p>
+        <p class="text-gray-400">FullStack Developer</p>
+        <NuxtLink to="https://github.com/GrafBunsen"><img src="~/assets/img/github-mark.png" alt="GitHub" height="20"></NuxtLink>
+      </div>
     </div>
 </template>
 
