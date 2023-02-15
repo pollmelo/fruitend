939ef99f80377e6a6fc244682cd4f276152b2653
GrafBunsen <103031906+GrafBunsen@users.noreply.github.com>
2023-02-15 11:27:39 +0100

    Feature/30 create about page (#35)
    * added team section
    
    * extracted components
    
    * fixed changes requested in pull requests
    
    * fixed HWR link
    
    * fixed typo in components/AboutText.vue
    
    Co-authored-by: EmmaB <66902725+emsbe@users.noreply.github.com>
    
    * fixed typo in components/AboutText.vue
    
    Co-authored-by: EmmaB <66902725+emsbe@users.noreply.github.com>
    
    * fixed typo in components/AboutText.vue
    
    Co-authored-by: EmmaB <66902725+emsbe@users.noreply.github.com>
    
    * added space on bottom of about page
    
    Co-authored-by: EmmaB <66902725+emsbe@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: EmmaB <66902725+emsbe@users.noreply.github.com>

---
+++ components/AboutTeam.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1 class="majorTxt m-4">Team</h1>
+  <div class="flex gap-10 justify-center mx-auto">
+    <div>
+      <img alt="icon of a person" src="~/assets/img/teamBlank.png">
+      <p class="minorTxt my-3">Martin</p>
+      <p class="text-gray-400">Technical Team Lead</p>
+      <a href="https://github.com/martincodes-de" target="_blank"><img alt="GitHub" height="24"
+                                                                       src="~/assets/img/github-mark.png"
+                                                                       width="24"></a>
+    </div>
+    <div>
+      <img alt="icon of a person" src="~/assets/img/teamBlank.png">
+      <p class="minorTxt my-3">Emma</p>
+      <p class="text-gray-400">Designerin / FullStack Developerin</p>
+      <a href="https://github.com/emsbe" target="_blank"><img alt="GitHub" height="24"
+                                                              src="~/assets/img/github-mark.png" width="24"></a>
+    </div>
+    <div>
+      <img alt="icon of a person" src="~/assets/img/teamBlank.png">
+      <p class="minorTxt my-3">Nik</p>
+      <p class="text-gray-400">FullStack Developer</p>
+      <a href="https://github.com/NiklasBGit" target="_blank"><img alt="GitHub" height="24"
+                                                                   src="~/assets/img/github-mark.png" width="24"></a>
+    </div>
+    <div>
+      <img alt="icon of a person" src="~/assets/img/teamBlank.png">
+      <p class="minorTxt my-3">Malte</p>
+      <p class="text-gray-400">FullStack Developer</p>
+      <a href="https://github.com/GrafBunsen" target="_blank"><img alt="GitHub" height="24"
+                                                                   src="~/assets/img/github-mark.png" width="24"></a>
+    </div>
+  </div>
+  <br>
+</template>
+
+<script>
+export default {
+  name: "AboutTeam"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/components/AboutText.vue
@@ -0,0 +1,11 @@
+<template>
+<aboutText/>
+</template>
+
+<script>
+definePageMeta({layout:''});
+</script>
+
+<style scoped>
+
+</style>
