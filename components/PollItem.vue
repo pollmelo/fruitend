fdd18e46bd67ede6ac5824b8deaddadc5857f5e7
GrafBunsen <103031906+GrafBunsen@users.noreply.github.com>
2023-03-02 13:46:35 +0100

    Revert "Feature/33 list of polls should be in order from newest to oldest" (#45)
    * reversed the order of the polls on index.vue
    
    * addressed changed requested in PR
    
    * Revert "Feature/33 list of polls should be in order from newest to oldest (#43)"
    
    This reverts commit f60b266026bac81c35069d74b13078718452c7eb.
    
    * feature now complete, maltes error is no more!
    
    ---------
    
    Co-authored-by: Niklas <niklas.bieda@gmail.com>

---
+++ components/PollItem.vue
@@ -0,0 +1,123 @@
+<template>
+    <br>
+    <div class="px-2">
+        <div class="flex flex-wrap -mx-2">
+            <div class="w-1/4">
+            </div>
+            <div class="w-1/2 flex-none">
+                <div class="container flex-wrap items-center bg-pomelo-grey w-1/4 p-6 min-w-fit">
+                    <div class="font-bold w-full p-2 bg-white border-2 border-pomelo-red">
+                      {{ this.name }}
+                    </div>
+                    <div class="h-60 mt-2 w-full p-2 bg-white border-2 border-pomelo-red">
+                        {{this.description}}
+                    </div>
+                    <div class="flex flex-row pt-10">
+                        <div class="flex-col w-1/5 mr-auto bg-white p-2">
+                            endet am: {{ new Date(this.endDate).toLocaleDateString(undefined, this.displayedDateOptions) }}
+                        </div>
+                        <div class="flex-col">
+                            <Icon :class="['text-green-900', 'hover:text-green-700']" :name="'bi:hand-thumbs-up-fill'" @click="addUpvote"
+                                :size="'2em'" />
+                            <label class="text-center">{{this.upvotes}}</label>
+                        </div>
+                        <div class="flex-col px-10">
+                            <Icon :class="['text-red-900', 'hover:text-red-700']" :name="'bi:hand-thumbs-down-fill'" @click="addDownvote"
+                                :size="'2em'" />
+                            <label class="text-center">{{ this.downvotes }}</label>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="w-1/4">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: ['id', 'name', 'description', 'endDate', 'upvoteCount', 'downvoteCount'],
+
+  mounted() {
+    this.upvotes = this.upvoteCount
+    this.downvotes = this.downvoteCount
+  },
+
+  data() {
+    return {
+      timestampFromNow: Date.now(),
+      timestampOfEndDate: Date.parse(this.endDate),
+      upvotes: 0,
+      downvotes: 0,
+      displayedDateOptions: {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      },
+    }
+  },
+
+  computed: {
+    urlToPoll() {
+      return `/polls/${this.id}`;
+    },
+
+    isVotable() {
+      return this.timestampOfEndDate > this.timestampFromNow;
+    },
+
+    endDatePrefix() {
+      return this.isVotable ? 'endet' : 'endete';
+    },
+
+    pollCreatorAvatarUrl() {
+      return `https://api.dicebear.com/5.x/micah/svg?seed=${this.id}&mouth=pucker,laughing,smile&size=64`;
+    },
+
+    trafficLightColor() {
+      return this.isVotable ? 'text-green-500' : 'text-red-500';
+    },
+  },
+
+  methods: {
+    async addUpvote() {
+      this.upvotes += 1
+      this.upvote()
+    },
+
+    async addDownvote() {
+      this.downvotes += 1
+      this.downvote()
+    },
+
+    async upvote() {
+      const {data, error} = await useFetch(() => `https://backberry.ddev.site/api/votes/create`, {
+        method: 'POST', 
+        initialCache: false,
+        body: {pollId: this.id, description: 1}
+      })
+      if (data) {
+        console.log(data)
+      }
+    },
+
+    async downvote() {
+      const {data, error} = await useFetch(() => `https://backberry.ddev.site/api/votes/create`, {
+        method: 'POST', 
+        initialCache: false,
+        body: {pollId: this.id, description: 0}
+      })
+      if (data) {
+        console.log(data)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
-      <div class="relative">
-        <img alt="background-banner" class="object-cover h-60 w-full" src="~/assets/img/banner.png"/>
-        <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          Jetzt abstimmen!</h1>
-      </div>
-      <div class="container py-10 mx-auto">
-        <CreatePollButton/>
-
-        <section class="mt-10">
-          <Icon v-if="polls == null" :class="['animate-spin', 'text-pomelo-red']" :name="'bx:loader-circle'" :size="'5em'" />
-          <PollListItem
-              v-for="poll in polls.slice().reverse()" :key="poll.id" :class="['mb-3.5']"
-              :id="poll.id" :name="poll.name" :description="poll.description" :end-date="poll.endDate"
-              :upvote-count="poll.upvotes" :downvote-count="poll.downvotes"
-          />
-        </section>
-      </div>
+  <div>
+    <div class="relative">
+      <img alt="background-banner" class="object-cover h-60 w-full" src="~/assets/img/banner.png"/>
+      <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        Jetzt abstimmen!</h1>
     </div>
+    <div class="container py-10 mx-auto">
+      <CreatePollButton/>
+
+      <section class="mt-10">
+        <Icon v-if="polls == null" :class="['animate-spin', 'text-pomelo-red']" :name="'bx:loader-circle'"
+              :size="'5em'"/>
+        <PollListItem
+            v-if="polls" v-for="poll in sortedPolls" :id="poll.id" :key="poll.id"
+            :class="['mb-3.5']" :description="poll.description" :downvote-count="poll.downvotes"
+            :end-date="poll.endDate"
+            :name="poll.name" :upvote-count="poll.upvotes"
+        />
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -26,22 +28,26 @@ export default {
     this.fetchPolls();
     document.title = "POLLmelo"
   },
-
   data() {
     return {
       polls: null,
     }
   },
 
+  computed: {
+    sortedPolls() {
+      return this.polls.sort((a, b) => Date.parse(b.endDate) - Date.parse(a.endDate))
+    }
+  },
+
   methods: {
     async fetchPolls() {
       const polls = await useFetch('https://backberry.ddev.site/api/polls/all');
-      this.polls = Array.from(polls.data.value);
+      this.polls = polls.data;
     }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
