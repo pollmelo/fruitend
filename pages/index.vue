1978bb23850a2c4596f0c0e8cd6b4cc0440aed57
GrafBunsen <103031906+GrafBunsen@users.noreply.github.com>
2023-03-02 10:19:57 +0100

    Revert "Feature/33 list of polls should be in order from newest to oldest (#43)"
    This reverts commit f60b266026bac81c35069d74b13078718452c7eb.

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
-      <div class="relative">
-        <img alt="background-banner" class="object-cover h-60 w-full" src="~/assets/img/banner.png"/>
-        <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          Jetzt abstimmen!</h1>
-      </div>
-      <div class="container py-10 mx-auto">
-        <CreatePollButton/>
-
-        <section class="mt-10">
-          <Icon v-if="polls == null" :class="['animate-spin', 'text-pomelo-red']" :name="'bx:loader-circle'" :size="'5em'" />
-          <PollListItem
-              v-for="poll in polls.slice().reverse()" :key="poll.id" :class="['mb-3.5']"
-              :id="poll.id" :name="poll.name" :description="poll.description" :end-date="poll.endDate"
-              :upvote-count="poll.upvotes" :downvote-count="poll.downvotes"
-          />
-        </section>
-      </div>
+  <div>
+    <div class="relative">
+      <img alt="background-banner" class="object-cover h-60 w-full" src="~/assets/img/banner.png"/>
+      <h1 class="absolute text-center text-5xl text-pomelo-red font-extrabold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        Jetzt abstimmen!</h1>
     </div>
+    <div class="container py-10 mx-auto">
+      <CreatePollButton/>
+
+      <section class="mt-10">
+        <Icon v-if="polls == null" :class="['animate-spin', 'text-pomelo-red']" :name="'bx:loader-circle'"
+              :size="'5em'"/>
+        <PollListItem
+            v-for="poll in polls" :id="poll.id" :key="poll.id"
+            :class="['mb-3.5']" :description="poll.description" :downvote-count="poll.downvotes"
+            :end-date="poll.endDate"
+            :name="poll.name" :upvote-count="poll.upvotes"
+        />
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,9 +38,9 @@ export default {
   methods: {
     async fetchPolls() {
       const polls = await useFetch('https://backberry.ddev.site/api/polls/all');
-      this.polls = Array.from(polls.data.value);
+      this.polls = polls.data;
     }
-  }
+  },
 }
 </script>
 
